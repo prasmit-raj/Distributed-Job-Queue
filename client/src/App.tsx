5e3b1563b84fe9fc997897ac40efc6b6c388cc20
Prasmit Raj <prasmitraj056@gmail.com>
2026-08-05 10:48:13 +0530

    Add login and signup pages with React Router

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
