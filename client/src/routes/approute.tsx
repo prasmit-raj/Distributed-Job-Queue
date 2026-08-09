e257c389947decea715cc448cabccc77b9027a1b
Prasmit Raj <prasmitraj056@gmail.com>
2026-08-09 21:19:10 +0530

    added singup otp for client side

--- client/src/routes/approute.tsx
+++ client/src/routes/approute.tsx
@@ -4,6 +4,7 @@ import { Routes, Route} from "react-router-dom";
 import Login from "../pages/login/login";
 import LoginOTP from "../pages/login/loginotp";
 import Signup from "../pages/signup/signup";
+import SignupOTP from "../pages/signup/signupotp";
 
 function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ function AppRoutes() {
       <Route path="/" element={<Login />} />
       <Route path="/loginotp" element={<LoginOTP />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/signupotp" element={<SignupOTP />} />
     </Routes>
    
   );
