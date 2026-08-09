498299cadc91c5926074243207bb148c90bd6eeb
Prasmit Raj <prasmitraj056@gmail.com>
2026-08-09 21:04:59 +0530

    added login otp

---
+++ client/src/pages/login/loginotp.tsx
@@ -0,0 +1,30 @@
+import Image from "../../assets/auth-bg.webp";
+import {Link} from "react-router-dom";
+function LoginOTP() {
+    return(
+        <div >
+          <img src={Image} alt="Background" className="relative w-screen h-screen bg-cover bg-center" />
+
+
+  <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white/10 backdrop-blur-md p-8 rounded-lg shadow-md w-[420px]">
+                
+        
+                 <div    className ="flex mx-4 my-8">
+            <label htmlFor="otp">Enter OTP :</label>
+            <input type="text" id="otp" placeholder="Enter your otp" className=" border rounded-lg mx-4" required />
+        </div>
+
+        <div className="flex justify-center items-center my-5">
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              <Link to="/dashboard">submit</Link>
+                
+            </button>
+                </div>
+
+                 </form>
+
+          </div>
+    );
+}
+
+export default LoginOTP;
--- client/src/routes/approute.tsx
+++ client/src/routes/approute.tsx
@@ -2,12 +2,14 @@
 import { Routes, Route} from "react-router-dom";
 
 import Login from "../pages/login/login";
+import LoginOTP from "../pages/login/loginotp";
 
 function AppRoutes() {
   return (
     
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/loginotp" element={<LoginOTP />} />
     </Routes>
    
   );
